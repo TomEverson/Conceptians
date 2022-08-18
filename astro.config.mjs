b61e7be126380289e043a16883629f817d517d6e
Tom Everson <studycafe.125@gmail.com>
2022-08-18 09:54:19 +0630

    Added Prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
- import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions';
+import prefetch from '@astrojs/prefetch';
 
 export default defineConfig({
    output: 'server',
    adapter: netlify(),
+   integrations: [prefetch()],
 });
