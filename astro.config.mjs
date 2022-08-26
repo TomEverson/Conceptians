e8c0e50ce35c32109d81cc61c6a2a69d05976834
Tom Everson <studycafe.125@gmail.com>
2022-08-26 09:46:05 +0630

    Tailwind Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 import prefetch from '@astrojs/prefetch';
-import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
    output: 'server',
    adapter: netlify(),
-   integrations: [prefetch(),react(),svelte()],
+   integrations: [prefetch(),svelte(),tailwind(
+      {
+         config: { path: './tailwind.config.js' },
+       }
+   )],
 });
